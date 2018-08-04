419967639eadefbb510b5fe8cceadc562f34b628
github username <github email address>
2018-08-03 21:07:08 -0400

    Set up file structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,25 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    use Rack::Flash
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secret'
   end
 
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
