3ecfa8f628cdbc7ce9b6b3acbab92785a6dc925c
github username <github email address>
2018-08-06 19:24:41 -0400

    Fixing up add events feature

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   get '/event/:venue_id/new' do
     if logged_in?
       @venue = Venue.find_by_id(params[:venue_id])
-      erb :'event/new'
+      erb :'events/new'
     else
       redirect to '/login'
     end
@@ -20,6 +20,28 @@ class EventsController < ApplicationController
       @venue.event << @event
       redirect to "/venues/#{@user.username}/#{@venue.id}"
     end
+  end
+
+  get '/event/:venue_id/:event_id/edit' do
+    @venue = Venue.find_by_id(params[:venue_id])
+    @event = Event.find_by_id(params[:event_id])
+    erb :'event/edit'
+  end
+
+  patch '/event/:venue_id/:event_id' do
+    @user = User.find_by_id(session[:user_id])
+    @venue = Venue.find_by_id(params[:venue_id])
+    @event = Event.find_by_id(params[:event_id])
+    @event.update(name: params[:name], date: params[:date],time: params[:time])
+    redirect to "/venues/#{@user.username}/#{@venue.id}"
+  end
+
+  delete '/event/:venue_id/:event_id/delete' do
+    @user = User.find_by_id(session[:user_id])
+    @venue = Venue.find_by_id(params[:venue_id])
+    @event = Event.find_by_id(params[:event_id])
+    @event.delete
+    redirect to "/venues/#{@user.username}/#{@venue.id}"
   end
 
 end
