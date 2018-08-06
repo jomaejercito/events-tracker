ac2ec0f1e178adc2d58266652b665a7a583513eb
github username <github email address>
2018-08-05 23:08:07 -0400

    Added venues controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -9,4 +9,39 @@ class VenuesController < ApplicationController
     end
   end
 
+  get '/venues/new' do
+    if logged_in?
+      erb :"venues/new"
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/venues' do
+    if params[:name].empty?
+      redirect to '/venues/new'
+    else
+      @user = User.find_by_id(session[:user_id])
+      @venue = Venue.create(params)
+      @user.venues << @venue
+      redirect to "/venues/#{@user.username}"
+    end
+  end
+
+  get '/venues/:username' do
+    @user = User.find_by(username: params[:username])
+    @venues = @user.venue
+    if logged_in? && current_user == @user
+      erb :"venue/show"
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/venues/:username/:venue_id' do
+    @user = User.find_by(username: params[:username])
+    @venue = Venue.find_by_id(params[:venue_id])
+    erb :"venues/index"
+  end
+
 end
