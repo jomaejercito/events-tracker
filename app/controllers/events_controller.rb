d7372d71b404aba653a95aa47beb284854d3ee22
github username <github email address>
2018-08-06 20:05:06 -0400

    Trying to figure out NoMethodError

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,14 +10,14 @@ class EventsController < ApplicationController
   end
 
   post '/event/:venue_id' do
-    if params[:name].empty?
+    if params[:name].empty? || params[:date].empty? || params[:time].empty?
       @venue = Venue.find_by_id(params[:venue_id])
       redirect to "/event/#{@venue.id}/new"
     else
       @user = User.find_by_id(session[:user_id])
       @venue = Venue.find_by_id(params[:venue_id])
-      @event = Event.create(name: params[:name], date: params[:date],time: params[:time])
-      @venue.event << @event
+      @event = Event.create(name: params[:name], date: params[:date], time: params[:time])
+      @venue.events << @event
       redirect to "/venues/#{@user.username}/#{@venue.id}"
     end
   end
@@ -25,14 +25,14 @@ class EventsController < ApplicationController
   get '/event/:venue_id/:event_id/edit' do
     @venue = Venue.find_by_id(params[:venue_id])
     @event = Event.find_by_id(params[:event_id])
-    erb :'event/edit'
+    erb :'events/edit'
   end
 
   patch '/event/:venue_id/:event_id' do
     @user = User.find_by_id(session[:user_id])
     @venue = Venue.find_by_id(params[:venue_id])
     @event = Event.find_by_id(params[:event_id])
-    @event.update(name: params[:name], date: params[:date],time: params[:time])
+    @event.update(name: params[:name], date: params[:date], time: params[:time])
     redirect to "/venues/#{@user.username}/#{@venue.id}"
   end
 
